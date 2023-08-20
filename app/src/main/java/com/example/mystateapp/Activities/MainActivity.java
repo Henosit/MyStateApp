cac747b795ca0475c7c07af785ca6027f3a7c9b6
Henosit <chenmoasis@gmail.com>
2023-08-20 18:47:22 +0300

    replaced the borders recycle view and all that with alert dialog

--- app/src/main/java/com/example/mystateapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/mystateapp/Activities/MainActivity.java
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.ContactsContract;
+import android.view.View;
 
+import com.example.mystateapp.Adapters.BorderAdapter;
 import com.example.mystateapp.Adapters.StateAdapter;
 import com.example.mystateapp.Models.State;
 import com.example.mystateapp.R;
@@ -16,9 +20,15 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private LinearLayoutManager layoutManager;
+    private RecyclerView recyclerView, bordersRecyclerView;
+    private LinearLayoutManager layoutManager, bordersLayoutManager;
     private StateAdapter adapter;
+
+    public BorderAdapter getBorderAdapter() {
+        return borderAdapter;
+    }
+
+    private BorderAdapter borderAdapter;
     private ArrayList<State> dataSet;
 
 
@@ -33,7 +43,32 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         dataSet = DataService.getArrState(); //an array that shrinks/grows in real time
-        adapter = new StateAdapter(dataSet);
+//        borderAdapter = new BorderAdapter(dataSet);
+        adapter = new StateAdapter(dataSet, this);
         recyclerView.setAdapter(adapter);
+//
+//        bordersRecyclerView = (RecyclerView) findViewById(R.id.bordersRecView);
+//        bordersLayoutManager = new LinearLayoutManager(this);
+//        bordersRecyclerView.setLayoutManager(bordersLayoutManager);
+//        bordersRecyclerView.setItemAnimator(new DefaultItemAnimator());
+//        bordersRecyclerView.setAdapter(borderAdapter);
     }
+
+    public void changeBordersRecyclerViewVisibility() {
+        int visibilityMode = bordersRecyclerView.getVisibility();
+        if (visibilityMode == View.GONE) {
+            bordersRecyclerView.setVisibility(View.VISIBLE);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    bordersRecyclerView.setVisibility(View.GONE);
+                    borderAdapter.clearBorders(); // Clear the borders data in BorderAdapter
+                }
+            }, 3000); // 3000 milliseconds = 3 seconds
+        } else if (visibilityMode == View.VISIBLE) {
+            bordersRecyclerView.setVisibility(View.GONE);
+            borderAdapter.clearBorders(); // Clear the borders data in BorderAdapter
+        }
+    }
+
 }
--- app/src/main/java/com/example/mystateapp/Adapters/StateAdapter.java
+++ app/src/main/java/com/example/mystateapp/Adapters/StateAdapter.java
@@ -1,25 +1,28 @@
 package com.example.mystateapp.Adapters;
 
+import android.os.Handler;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGImageView;
 import com.caverock.androidsvg.SVGParseException;
+import com.example.mystateapp.Activities.MainActivity;
+
 import com.example.mystateapp.Models.State;
 import com.example.mystateapp.R;
-
-import org.w3c.dom.Text;
+import com.example.mystateapp.Services.DataService;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +30,13 @@ import java.util.List;
 public class StateAdapter extends RecyclerView.Adapter<StateAdapter.MyViewHolder> {
 
     private ArrayList<State> dataSet;
-
-    public StateAdapter(ArrayList<State> dataSet) {
+    private MainActivity mainActivity;
+    private BorderAdapter borderAdapter;
+    private int clickedPosition = -1; // Track the clicked position
+    public StateAdapter(ArrayList<State> dataSet, MainActivity mainActivity) {
         this.dataSet = dataSet;
+        this.mainActivity = mainActivity;
+        this.borderAdapter = mainActivity.getBorderAdapter();
     }
 
     @NonNull
@@ -47,15 +54,12 @@ public class StateAdapter extends RecyclerView.Adapter<StateAdapter.MyViewHolder
         // describes how we present each arr element in the recycleView
         TextView textViewName = holder.textName;
         TextView textViewNativeName = holder.textNativeName;
-        TextView textViewBorders = holder.textBorders;
+//        TextView textViewBorders = holder.textBorders;
         SVGImageView svgImageViewFlag = holder.svgImageFlag;
 
         textViewName.setText("Name: " + dataSet.get(position).getName());
         textViewNativeName.setText("Native Name: " + dataSet.get(position).getNativeName());
         List<String> borders = dataSet.get(position).getBorders();
-        if (borders != null) {
-            textViewBorders.setText("Borders: " + borders);
-        }
 
         // Load and display SVG flag image
         String svgURL = dataSet.get(position).getFlag();
@@ -70,6 +74,27 @@ public class StateAdapter extends RecyclerView.Adapter<StateAdapter.MyViewHolder
         } catch (SVGParseException e) {
             throw new RuntimeException(e);
         }
+        svgImageViewFlag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<String> borders = dataSet.get(position).getBorders();
+                if (borders != null && !borders.isEmpty()) {
+                    // Convert the list of borders to a single string
+                    String bordersString = TextUtils.join(", ", borders);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+                    View dialogView = LayoutInflater.from(mainActivity).inflate(R.layout.popup_layout, null);
+                    TextView textViewBordersTitle = dialogView.findViewById(R.id.textViewBordersTitle);
+                    TextView textViewBorders = dialogView.findViewById(R.id.textViewBorders);
+
+                    textViewBorders.setText(bordersString);
+
+                    builder.setView(dialogView);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
     }
 
     @Override
@@ -80,14 +105,14 @@ public class StateAdapter extends RecyclerView.Adapter<StateAdapter.MyViewHolder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // imports all existing variables in our xml for a single row
 
-        TextView textName, textNativeName, textBorders;
+        TextView textName, textNativeName; //textBorders
         SVGImageView svgImageFlag;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             textName = itemView.findViewById(R.id.textViewName);
             textNativeName = itemView.findViewById(R.id.textViewNativeName);
-            textBorders = itemView.findViewById(R.id.textViewBorders);
+//            textBorders = itemView.findViewById(R.id.textViewBorders);
             svgImageFlag = itemView.findViewById(R.id.svgImageViewFlag);
         }
     }
